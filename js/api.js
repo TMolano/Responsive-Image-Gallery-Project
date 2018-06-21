534a8f54299746d3c40fb0e647275997a611f533
Trevor M <temolano@fullsail.edu>
2018-06-21 00:21:05 -0400

    Image-Gallery-Ver1

---
+++ js/api.js
@@ -0,0 +1,35 @@
+const key = 'a76888a1a0e311b48583aeb7d4071f02209f8096af0b11a40bb385c07a2915b9';
+const api_endpoint = 'https://api.unsplash.com/photos/';
+
+
+//Fetch API data
+
+
+//Filter Comics based off Search Input
+
+
+
+    const url = api_endpoint+'?client_id='+key;
+
+    fetch(url)
+
+        .then(results => results.json())
+
+        .then(resultsJSON => {
+            //Insert API data
+            let newContent = '';
+            for(let i = 0; i < resultsJSON.length; i++)
+            {
+                newContent += '<li class="photos"><article>'+ '<img src="' + resultsJSON[i].urls.small + 'alt=image of comic art">';
+                newContent += '<div id="hover"><p id="user">' + resultsJSON[i].user.name + '</p>';
+                newContent += '<p id="likes">' + resultsJSON[i].likes + '</p></div></article></li>';
+            }
+            let apiResults = document.querySelector('ul#photoGallery');
+            apiResults.innerHTML = newContent;
+            console.log(resultsJSON);
+
+        })
+
+        .catch( error => {
+            console.log('An Error Occurred:', error)
+        });

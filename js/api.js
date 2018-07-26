023a7f69931fc3fd51e751f5c0d6a9b9f9dce75a
Trevor M <temolano@fullsail.edu>
2018-07-26 00:05:12 -0400

    JS Responsiveness changes

--- js/api.js
+++ js/api.js
@@ -3,31 +3,33 @@ const api_endpoint = 'https://api.unsplash.com/photos/';
 
 //Fetch API data
 
-    const url = api_endpoint+'?client_id='+key;
+const url = api_endpoint+'?client_id='+key;
 
-    fetch(url)
+fetch(url)
 
-        .then(results => results.json())
+    .then(results => results.json())
 
-        .then(resultsJSON => {
-            //Insert API data
-            let newContent = '';
+    .then(resultsJSON => {
+        //Insert API data
+        let newContent = '';
 
-            for(let i = 0; i < resultsJSON.length; i++)
-            {
-                newContent += '<li class="photos"><article>'+ '<img  src="' + resultsJSON[i].urls.small + '">';
-                newContent += '<div id="hover"><p id="user">' + resultsJSON[i].user.name + '</p>';
-                newContent += '<p id="likes">' + resultsJSON[i].likes + '</p></div></article></li>';
-            }
-            let apiResults = document.querySelector('ul#photoGallery');
-            apiResults.innerHTML = newContent;
-            console.log(resultsJSON);
+        for(let i = 0; i < resultsJSON.length; i++)
+        {
+            newContent += '<li class="photos"><article>'+ '<img  src="' + resultsJSON[i].urls.small + '" ' +
+                'srcset="' + resultsJSON[i].urls.small + ' 400w, https://www.tcgplayer.com/Content/images/tcgplayer-logo-color_320x120.png 800w" ' +
+                'sizes="(max-width:300px) 100vw, (max-width:1000px) 10vw"/>';
+            newContent += '<div id="hover"><p id="user">' + resultsJSON[i].user.name + '</p>';
+            newContent += '<p id="likes">' + resultsJSON[i].likes + '</p></div></article></li>';
+        }
+        let apiResults = document.querySelector('ul#photoGallery');
+        apiResults.innerHTML = newContent;
+        console.log(resultsJSON);
 
-        })
+    })
 
-        .catch( error => {
-            console.log('An Error Occurred:', error)
-        });
+    .catch( error => {
+        console.log('An Error Occurred:', error)
+    });
 
 
 const globe = document.getElementById("logo");
@@ -37,7 +39,7 @@ globe.addEventListener("click", function () {
     if(globe.className === "spin"){
         globe.classList.remove("spin");
     }
-else
-    globe.classList.add("spin");
+    else
+        globe.classList.add("spin");
 
 });
